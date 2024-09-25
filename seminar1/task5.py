dd54cc2197d7763afba233201c077216b2e9cf0e
Rustam Muratov <rusramicm@gmail.com>
2024-09-25 10:16:40 +0300

    add seminar1

---
+++ seminar1/task5.py
@@ -0,0 +1,17 @@
+''' a = 5
+while a > 0:
+    print(a)
+    if a == 3:
+        break
+    a  -= 1
+else:
+    print("Завершение цикла не по break")
+
+my_str ="ajasjdskf"
+for el in my_str:
+    print(el, end = " ") '''
+my_str = "ajasjdskf"
+i = 0
+while i < len(my_str):
+    print(my_str[i])
+    i += 1
